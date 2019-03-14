a4608738902363061a74800bb2872223e8d807ee
vinicius-santos <vinicius_bauer@hotmail.com>
2019-03-14 13:50:39 -0300

    update moongose processingchain
    update moongose processingchain

--- services/organizations.mongoose.js
+++ services/organizations.mongoose.js
@@ -81,6 +81,8 @@ var OrganizationSchema = new mongoose.Schema({
     {
       name: String,
       description: String,
+      date: Date,
+      active: Boolean,
       fixedCost: [{
         name: String,
         date: [Date],
@@ -628,6 +630,34 @@ organizations.route('/add/pricing/:id').post(function (req, res, next) {
     }
   });
 });
+
+organizations.route('/processingchain/:id').get(function (req, res, next) {
+  organizationModel.findById(req.params.id, function (err, org) {
+    if (err) {
+      return next(new Error(res.status(400).send('ERE008')));
+    } else {
+      res.json(org.processingChain);
+    }
+  });
+});
+
+organizations.route('/update/processingchain/:id').put(function (req, res, next) {
+  organizationModel.findById(req.params.id, function (err, org) {
+    if (!org) return next(new Error(res.status(400).send('ERE005')));
+    else {
+      org.processingChain = req.body;
+      org
+        .update(org)
+        .then(org => {
+          res.json('SRE001');
+        })
+        .catch(err => {
+          return next(new Error(res.status(400).send('ERE006')));
+        });
+    }
+  });
+});
+
 
 organizations.route('/fixedcost/:id').get(function (req, res, next) {
   organizationModel.findById(req.params.id, function (err, org) {
