b7f59e8ec2db0c6ef6e2cd09ecb49fe53417f484
vinicius <vinicius_bauer@gmail.com>
2019-04-25 01:38:11 -0300

    implementando segurança de não duplicidade de email
    implementando segurança de não duplicidade de email

--- models/customer.model.js
+++ models/customer.model.js
@@ -15,5 +15,23 @@ model = [{
         county: String,
         complement: String
     },
-    materials: [String]
+    materials: [String],
+    scheduling: [{
+        date: Date,
+        hour: Date,
+        location: {
+            state: String,
+            cep: String,
+            publicPlace: String,
+            neighborhood: String,
+            number: Number,
+            county: String,
+            complement: String
+        },
+        weight: Number,
+        description: String,
+        quantity: Number,
+        picture: String
+    }]
+
 }]
