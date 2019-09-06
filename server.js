5413fba155b8ad51deecb6968333864dbbc4f331
vinicius <viniciusdfgr@gmail.com>
2019-09-06 00:26:03 -0300

    modificando porta para rodar api e projeto
    modificando porta para rodar api e projeto

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 const app = require('./services/organizations.mongoose');
 
-const port = process.env.PORT || 32546;
+const port = process.env.PORT || 42546;
 app.listen(port, () => {
     console.info(`Api inicializada com sucesso na porta ${port}`);
 });
